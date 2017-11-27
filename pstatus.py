0620d602c2b6468b4f89053d70038fca43fb1416
relam <relam@rcschools.us>
2017-11-27 16:36:57 -0500

    added oids

--- pstatus.py
+++ pstatus.py
@@ -8,6 +8,20 @@ oid_ss_magenta = '1.3.6.1.2.1.43.11.1.1.9.1.2'
 oid_ss_yellow = '1.3.6.1.2.1.43.11.1.1.9.1.3'
 oid_ss_black = '1.3.6.1.2.1.43.11.1.1.9.1.4'
 
+# 0 - 100 (to the nearest 10%)
+oid_ricoh_black = '1.3.6.1.4.1.367.3.2.1.2.24.1.1.5.1'
+oid_ricoh_cyan = '1.3.6.1.4.1.367.3.2.1.2.24.1.1.5.2'
+oid_ricoh_magenta = '1.3.6.1.4.1.367.3.2.1.2.24.1.1.5.3'
+oid_ricoh_yellow = '1.3.6.1.4.1.367.3.2.1.2.24.1.1.5.4'
+
+# 0 = ok 9 = empty
+oid_ricoh_tray1 = '1.3.6.1.4.1.367.3.2.1.2.20.2.2.1.11.2.1'
+oid_ricoh_tray2 = '1.3.6.1.4.1.367.3.2.1.2.20.2.2.1.11.2.2'
+oid_ricoh_tray3 = '1.3.6.1.4.1.367.3.2.1.2.20.2.2.1.11.2.3'
+oid_ricoh_tray4 = '1.3.6.1.4.1.367.3.2.1.2.20.2.2.1.11.2.4'
+
+oid_ricoh_total_pcount = '1.3.6.1.4.1.367.3.2.1.2.19.1.0'
+
 printer_description = ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)
 
 def SamsungTonerPercantage(val):
@@ -16,13 +30,13 @@ def SamsungTonerPercantage(val):
 errorIndication, errorStatus, errorIndex, varBinds = next(
     getCmd(SnmpEngine(),
            CommunityData('public'),
-           UdpTransportTarget(('172.16.3.5', 161)),
+           UdpTransportTarget(('172.19.3.16', 161)),
            ContextData(),
-           ObjectType(ObjectIdentity('1.3.6.1.2.1.43.11.1.1.9.1.4')), #black
-           ObjectType(ObjectIdentity('1.3.6.1.2.1.43.11.1.1.9.1.1')), #cyan
-           ObjectType(ObjectIdentity('1.3.6.1.2.1.43.11.1.1.9.1.2')), #magenta
-           ObjectType(ObjectIdentity('1.3.6.1.2.1.43.11.1.1.9.1.3')), #yellow
-           ObjectType(ObjectIdentity(oid_ss_max_capacity)))
+           ObjectType(ObjectIdentity(oid_ricoh_black)),
+           ObjectType(ObjectIdentity(oid_ricoh_cyan)),
+           ObjectType(ObjectIdentity(oid_ricoh_magenta)),
+           ObjectType(ObjectIdentity(oid_ricoh_yellow)),
+           ObjectType(ObjectIdentity(oid_ricoh_total_pcount)))
 )
 
 if errorIndication:
