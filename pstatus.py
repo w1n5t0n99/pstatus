a3fbc50f32f6f77d7bdea177d542a7357e4893f0
relam <relam@rcschools.us>
2017-12-01 16:29:24 -0500

    added tk support

--- pstatus.py
+++ pstatus.py
@@ -3,11 +3,24 @@ import tkinter as tk
 import printer_query
 import printer_db
 
+root = tk.Tk()
+
 printers = printer_db.LoadDB('printers.txt')
 
 for p in printers:
     pres = printer_query.QueryPrinter(p)
     if '_color' in pres.type:
-        print('{0} model: {5} black: {1} cyan: {2} magenta: {3} yellow: {4}'.format(pres.name, pres.black, pres.cyan, pres.magenta, pres.yellow, pres.model) )
+        s = tk.StringVar()
+        s.set('{0} model: {5} black: {1} cyan: {2} magenta: {3} yellow: {4}'.format(pres.name, pres.black, pres.cyan,
+                                                                                    pres.magenta,
+                                                                                    pres.yellow,
+                                                                                    pres.model))
+        w = tk.Label(root, textvariable=s)
+        w.pack()
     else:
-        print('{0} model: {2} black: {1}'.format(pres.name, pres.black, pres.model))
+        s = tk.StringVar()
+        s.set('{0} model: {2} black: {1}'.format(pres.name, pres.black, pres.model))
+        w = tk.Label(root, textvariable=s)
+        w.pack()
+
+root.mainloop()
