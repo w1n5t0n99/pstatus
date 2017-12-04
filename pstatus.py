05517fa5aa45c8a2709ce1408fd4c14206d6556e
relam <relam@rcschools.us>
2017-12-04 13:57:11 -0500

    refactor gui code

--- pstatus.py
+++ pstatus.py
@@ -6,40 +6,53 @@ import printer_db
 
 root = tk.Tk()
 root.title('Printer Status')
+root.minsize(width=300, height= 500)
 
 printers = printer_db.LoadDB('printers.txt')
 
+def AddTonerLabel(level, row, column):
+    level_width = 5
+    b = tk.StringVar()
+    if isinstance(level, int):
+        b.set('{0}'.format(level))
+        tk.Label(root, textvariable=b, width=level_width,
+                 fg='black' if abs(level) >= 10 else '#ff3232').grid(row=row, column=column)
+    else:
+        b.set(level)
+        tk.Label(root, textvariable=b, width=level_width).grid(row=row, column=column)
+
 def AddLabels(printers):
     r = 0
+    level_width = 6
+
+    tk.Label(root, text='Name', bg='gray').grid(row=0, column=0)
+    tk.Label(root, text='Black', width=level_width, bg='gray').grid(row=0, column=1)
+    tk.Label(root, text='Cyan', width=level_width, bg='#00ffff').grid(row=0, column=2)
+    tk.Label(root, text='Magenta', width=level_width, bg='#ff00ff').grid(row=0, column=3)
+    tk.Label(root, text='Yellow', width=level_width, bg='#ffff00').grid(row=0, column=4)
+    r+=1
+
     for p in printers:
         pres = printer_query.QueryPrinter(p)
         if '_color' in pres.type:
             m = tk.StringVar()
             m.set('{0}'.format(pres.name))
             tk.Label(root, textvariable=m).grid(row=r, column=0)
-            b=tk.StringVar()
-            b.set('{0}'.format(pres.black))
-            tk.Label(root, textvariable=b).grid(row=r, column=1)
-            c = tk.StringVar()
-            c.set('{0}'.format(pres.cyan))
-            tk.Label(root, textvariable=c, bg ="#00ffff").grid(row=r, column=2)
-            m = tk.StringVar()
-            m.set('{0}'.format(pres.magenta))
-            tk.Label(root, textvariable=m, bg="#ff00ff").grid(row=r, column=3)
-            y = tk.StringVar()
-            y.set('{0}'.format(pres.yellow))
-            tk.Label(root, textvariable=y, bg="#ffff00").grid(row=r, column=4)
+
+            AddTonerLabel(pres.black, row=r, column=1)
+            AddTonerLabel(pres.cyan, row=r, column=2)
+            AddTonerLabel(pres.magenta, row=r, column=3)
+            AddTonerLabel(pres.yellow, row=r, column=4)
             r+=1
         else:
             m = tk.StringVar()
             m.set('{0}'.format(pres.name))
             tk.Label(root, textvariable=m).grid(row=r, column=0)
-            b = tk.StringVar()
+
             if pres.black != -3:
-                b.set('{0}'.format(pres.black))
+                AddTonerLabel(pres.black, row=r, column=1)
             else:
-                b.set('OK')
-            tk.Label(root, textvariable=b).grid(row=r, column=1)
+                AddTonerLabel('OK', row=r, column=1)
             r += 1
 
 threading.Thread(target=AddLabels, args=(printers,)).start()
