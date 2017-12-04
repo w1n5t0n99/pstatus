c14ca518ed641da882ad2b5a09a6e7f0bfceb27c
relam <relam@rcschools.us>
2017-12-04 11:42:33 -0500

    added async tk support

--- pstatus.py
+++ pstatus.py
@@ -1,26 +1,46 @@
 import tkinter as tk
+import threading
 
 import printer_query
 import printer_db
 
 root = tk.Tk()
+root.title('Printer Status')
 
 printers = printer_db.LoadDB('printers.txt')
 
-for p in printers:
-    pres = printer_query.QueryPrinter(p)
-    if '_color' in pres.type:
-        s = tk.StringVar()
-        s.set('{0} model: {5} black: {1} cyan: {2} magenta: {3} yellow: {4}'.format(pres.name, pres.black, pres.cyan,
-                                                                                    pres.magenta,
-                                                                                    pres.yellow,
-                                                                                    pres.model))
-        w = tk.Label(root, textvariable=s)
-        w.pack()
-    else:
-        s = tk.StringVar()
-        s.set('{0} model: {2} black: {1}'.format(pres.name, pres.black, pres.model))
-        w = tk.Label(root, textvariable=s)
-        w.pack()
+def AddLabels(printers):
+    r = 0
+    for p in printers:
+        pres = printer_query.QueryPrinter(p)
+        if '_color' in pres.type:
+            m = tk.StringVar()
+            m.set('{0}'.format(pres.name))
+            tk.Label(root, textvariable=m).grid(row=r, column=0)
+            b=tk.StringVar()
+            b.set('{0}'.format(pres.black))
+            tk.Label(root, textvariable=b).grid(row=r, column=1)
+            c = tk.StringVar()
+            c.set('{0}'.format(pres.cyan))
+            tk.Label(root, textvariable=c, bg ="#00ffff").grid(row=r, column=2)
+            m = tk.StringVar()
+            m.set('{0}'.format(pres.magenta))
+            tk.Label(root, textvariable=m, bg="#ff00ff").grid(row=r, column=3)
+            y = tk.StringVar()
+            y.set('{0}'.format(pres.yellow))
+            tk.Label(root, textvariable=y, bg="#ffff00").grid(row=r, column=4)
+            r+=1
+        else:
+            m = tk.StringVar()
+            m.set('{0}'.format(pres.name))
+            tk.Label(root, textvariable=m).grid(row=r, column=0)
+            b = tk.StringVar()
+            if pres.black != -3:
+                b.set('{0}'.format(pres.black))
+            else:
+                b.set('OK')
+            tk.Label(root, textvariable=b).grid(row=r, column=1)
+            r += 1
 
+threading.Thread(target=AddLabels, args=(printers,)).start()
 root.mainloop()
