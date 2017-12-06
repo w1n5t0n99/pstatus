9c1330fe09865f101d4dd65b3eaca2472fc96615
relam <relam@rcschools.us>
2017-12-06 13:42:28 -0500

    error check in file read

--- printer_db.py
+++ printer_db.py
@@ -10,6 +10,8 @@ def LoadDB(file_name):
                 d = shlex.split(line)
                 if len(d) == 3:
                     printers.append((d[0], d[1], d[2]))
+                else:
+                    printers.append(None, None, None)
 
         return printers
     else:
