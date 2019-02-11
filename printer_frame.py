6b212a1fda218bd3029a09eb3eaac8e470f8f49e
w1n5t0n99 <reedelam@gmail.com>
2019-02-11 11:51:06 -0500

    refactor printer frame widget

---
+++ printer_frame.py
@@ -0,0 +1,146 @@
+from tkinter import *
+import tkinter.messagebox
+
+import printer as pr
+import functools
+
+
+
+class PrinterFrame:
+    '''tkinter frame to display printer objects'''
+
+    def __init__(self, parent, grid_row, grid_col):
+        self._printer_frame = Frame(parent)
+        self._printer_frame.grid(row=grid_row, column=grid_col, pady=(5, 0), sticky=E + N + W + S)
+        self._printer_frame.grid_rowconfigure(0, weight=1)
+        self._printer_frame.grid_columnconfigure(0, weight=1)
+
+        self._printer_canvas = Canvas(self._printer_frame)
+        self._printer_canvas.grid(row=0, column=0, sticky=E + N + W + S)
+        self._printer_canvas.grid_rowconfigure(0, weight=1)
+        self._printer_canvas.grid_columnconfigure(0, weight=1)
+
+        self.vsb = Scrollbar(self._printer_frame, orient="vertical", command=self._printer_canvas.yview)
+        self.vsb.grid(row=0, column=1, sticky=N + S)
+
+
+    def _set_scroll_region(self):
+        scroll_height = 0
+        for p in self._printer_rows:
+            scroll_height += p[0].winfo_height()
+
+        self._printer_canvas.config(scrollregion=(0, 0, 0, scroll_height))
+
+
+    def _set_frame_width(self, event):
+        canvas_width = event.width
+        self._printer_canvas.itemconfigure(self._printer_canvas_window, width=canvas_width)
+
+
+    def _create_printer_rows(self):
+        f = self._printer_list_frame
+        self._printer_rows = [(Button(f), Label(f)) for p in self._printers]
+
+        i = 0
+        for p in self._printers:
+            self._printer_rows[i][0].config(text=("{}".format(p.name)), command=functools.partial(self._printer_detail_mb, i))
+            self._printer_rows[i][0].grid(row=i, column=0, padx=(15, 0), sticky=N + S + E + W)
+            f.grid_rowconfigure(i, weight=1)
+
+            if p.black is None:
+                self._printer_rows[i][1].config(text="ERROR")
+            elif p.cyan is not None:
+                self._printer_rows[i][1].config(text="B: {} C: {} M: {} Y: {}".format(p.black, p.cyan, p.magenta, p.yellow))
+            else:
+                self._printer_rows[i][1].config(text="B: {}".format(p.black))
+
+            self._printer_rows[i][1].grid(row=i, column=1, sticky=N + S + E + W)
+
+            i += 1
+
+
+    def _printer_detail_mb(self, row):
+        p = self._printers[row]
+
+        if p.black is None:
+            msg = "ERROR"
+        elif p.cyan is None:
+            msg = "black: {}\nfuser: {}\ntray 1 roller: {}\ntray 1 torque limiter: {}\nerror state: {}".format(p.black,
+                                                                                                               p.fuser,
+                                                                                                               p.tr1_roller,
+                                                                                                               p.tr1_torque_limiter,
+                                                                                                               p.error_state)
+        else:
+            msg = "black: {} cyan: {} magenta: {} yellow: {}\nfuser: {}\ntray 1 roller: {}\ntransfer belt: {}\nerror state: {}".format(
+                p.black,
+                p.cyan,
+                p.magenta,
+                p.yellow,
+                p.fuser,
+                p.tr1_roller,
+                p.tr_belt,
+                p.error_state)
+
+        tkinter.messagebox.showinfo(title="{}".format(p.name), message=msg)
+
+    def _resize_printer_frame(self, num_to_show):
+
+        if len(self._printer_rows) <= num_to_show:
+            n = len(self._printer_rows)
+        else:
+            n = num_to_show
+
+        first_n_columns_width = self._printer_rows[0][0].winfo_width() + self._printer_rows[0][1].winfo_width()
+        first_n_rows_height = 0
+
+        for i in range(0, n):
+            first_n_rows_height += self._printer_rows[i][0].winfo_height()
+
+        self._printer_canvas.config(width=first_n_columns_width + self.vsb.winfo_width(), height=first_n_rows_height)
+
+
+    def set_printers(self, printers):
+        self._printers = printers
+        self._printer_list_frame = Frame(self._printer_canvas)
+        self._printer_list_frame.columnconfigure(0, weight=1)
+        self._printer_list_frame.columnconfigure(1, weight=1)
+
+        self._printer_canvas_window = self._printer_canvas.create_window((0, 0), window=self._printer_list_frame, anchor=N + W)
+        self._printer_canvas.bind('<Configure>', self._set_frame_width)
+
+        self._create_printer_rows()
+        self._printer_frame.update()
+
+        self._resize_printer_frame(14)
+        self._printer_canvas.update_idletasks()
+        self._set_scroll_region()
+        self._printer_canvas.config(yscrollcommand=self.vsb.set)
+
+    def update_printers(self):
+
+        i=0
+        for p in self._printers:
+            if p.black is None:
+                self._printer_rows[i][1].config(text="ERROR")
+            elif p.cyan is not None:
+                self._printer_rows[i][1].config(text="B: {} C: {} M: {} Y: {}".format(p.black, p.cyan, p.magenta,
+                                                                                      p.yellow))
+            else:
+                self._printer_rows[i][1].config(text="B: {}".format(p.black))
+
+            i += 1
+
+    def update_row(self, row):
+        if row >= len(self._printer_rows):
+            assert IndexError
+
+        p = self._printers[row]
+        if p.black is None:
+            self._printer_rows[row][1].config(text="ERROR")
+        elif p.cyan is not None:
+            self._printer_rows[row][1].config(text="B: {} C: {} M: {} Y: {}".format(p.black, p.cyan, p.magenta,
+                                                                                  p.yellow))
+        else:
+            self._printer_rows[row][1].config(text="B: {}".format(p.black))
+
+
--- pstatus.py
+++ pstatus.py
@@ -6,6 +6,7 @@ import functools
 import printer_db as db
 import printer as pr
 import copier as cp
+import printer_frame as pf
 
 printers = []
 printer_button_col = []
@@ -29,104 +30,20 @@ def query_printer(printer):
         printer.clear()
 
 
-def create_printer_name_column(printers, frame):
-    prows = [Button() for p in printers]
+def update_if_thread_finished(printer_threads, printer_frame):
+    alive = True
+    while alive == True:
+        i = 0
+        for p in printer_threads:
+            cur_alive = False
+            if p.is_alive():
+                cur_alive = True
+            else:
+                printer_frame.update_row(i)
 
-    i = 0
-    for p in printers:
-        prows[i] = Button(frame, text=("{}".format(p.name)), command= functools.partial(printer_detail_mb, i))
-        prows[i].grid(row=i, column=0, sticky=N + S + E + W)
-        frame.grid_rowconfigure(i, weight=1)
-        i+=1
+            i+=1
+        alive = cur_alive
 
-    return prows
-
-
-def create_printer_info_column(printers, frame):
-    prows = [Label() for p in printers]
-
-    i = 0
-    for p in printers:
-        if p.black is None:
-            prows[i] = Label(frame, text="ERROR")
-        elif p.cyan is not None:
-            prows[i] = Label(frame, text="b: {} c: {} m: {} y : {}".format(p.black, p.cyan, p.magenta, p.yellow))
-        else:
-            prows[i] = Label(frame, text="b: {}".format(p.black))
-
-        prows[i].grid(row=i, column=1, sticky=N + S + E + W)
-        i += 1
-
-    return prows
-
-
-
-def resize_printer_frame(frame, name_column, info_column, num_to_show):
-
-    if len(name_column) != len(info_column):
-        raise ValueError
-
-    if len(name_column) <= num_to_show:
-        n = len(name_column)
-    else:
-        n = num_to_show
-
-    first_n_columns_width = name_column[0].winfo_width() + info_column[0].winfo_width()
-    first_n_rows_height = 0
-
-    for i in range(0, n):
-        first_n_rows_height += name_column[i].winfo_height()
-
-    frame.config(width=first_n_columns_width + vsb.winfo_width(), height=first_n_rows_height)
-
-
-def refresh_printer_row(row):
-    p = printers[row]
-    if p.cyan is not None:
-        printer_label_col[row].config( text="b: {} c: {} m: {} y : {}".format(p.black, p.cyan, p.magenta, p.yellow))
-    else:
-        printer_label_col[row].config(text="b: {}".format(p.black))
-
-
-def printer_detail_mb(row):
-    p = printers[row]
-
-    if p.black is None:
-        msg = "ERROR"
-    elif p.cyan is None:
-        msg = "black: {}\nfuser: {}\ntray 1 roller: {}\ntray 1 torque limiter: {}\nerror state: {}".format(p.black,
-                                                                                                           p.fuser,
-                                                                                                           p.tr1_roller,
-                                                                                                           p.tr1_torque_limiter,
-                                                                                                           p.error_state)
-    else:
-        msg = "black: {} cyan: {} magenta: {} yellow: {}\nfuser: {}\ntray 1 roller: {}\ntransfer belt: {}\nerror state: {}".format(p.black,
-                                                                                                                                   p.cyan,
-                                                                                                                                   p.magenta,
-                                                                                                                                   p.yellow,
-                                                                                                                                   p.fuser,
-                                                                                                                                   p.tr1_roller,
-                                                                                                                                   p.tr_belt,
-                                                                                                                                   p.error_state)
-
-    tkinter.messagebox.showinfo(title="{}".format(p.name), message=msg)
-
-def set_scroll_region(printer_col, canvas):
-    scroll_height = 0
-    for p in printer_col:
-        scroll_height += p.winfo_height()
-
-    canvas.config(scrollregion=(0, 0, 0, scroll_height))
-
-
-def frame_width(event):
-    canvas_width = event.width
-    canvas.itemconfigure(canvas_window, width=canvas_width)
-
-
-def on_frame_configure(event):
-    #canvas.config(scrollregion=canvas.bbox("all"))
-    set_scroll_region(printer_button_col, canvas)
 
 
 if __name__ == "__main__":
@@ -137,11 +54,6 @@ if __name__ == "__main__":
 
     for p in printers:
         printer_threads.append(threading.Thread(target=query_printer, args=(p,)))
-
-    for p in printer_threads:
-        p.start()
-    for p in printer_threads:
-        p.join()
 
     root = Tk()
     root.title("printer status")
@@ -160,41 +72,18 @@ if __name__ == "__main__":
     label = Label(main_frame, text="Printers", bg='gray')
     label.grid(row=0, column=0, pady=(5, 0), padx=(5,5), sticky=E + N + W + S)
 
-    ref_button = Button(main_frame, text="Refresh", bg='light gray')
+    printer_frame = pf.PrinterFrame(main_frame, 1, 0)
+    printer_frame.set_printers(printers)
+
+    ref_button = Button(main_frame, text="Refresh", bg='light gray', command=printer_frame.update_printers)
     ref_button.grid(row=2, column=0, pady=(5, 0), padx=(5, 0), sticky=W)
 
-    frame_canvas = Frame(main_frame, bg="green")
-    frame_canvas.grid(row=1, column=0, pady=(5, 0), sticky=E + N + W + S)
-    frame_canvas.grid_rowconfigure(0, weight=1)
-    frame_canvas.grid_columnconfigure(0, weight=1)
+    for p in printer_threads:
+        p.start()
+  #  for p in printer_threads:
+  #     p.join()
 
-    # frame canvas children =====================================
-    canvas = Canvas(frame_canvas)
-    canvas.grid(row=0, column=0, sticky=E + N + W + S)
-    canvas.grid_rowconfigure(0, weight=1)
-    canvas.grid_columnconfigure(0, weight=1)
+    printer_frame.update_printers()
 
-    vsb = Scrollbar(frame_canvas, orient="vertical", command=canvas.yview)
-    vsb.grid(row=0, column=1, sticky=N + S)
-
-    # canvas children ==============================================
-
-    printers_frame = Frame(canvas, bg="blue")
-    printers_frame.columnconfigure(0, weight=1)
-    printers_frame.columnconfigure(1, weight=1)
-
-    canvas_window = canvas.create_window((0, 0), window=printers_frame, anchor=N + W)
-
-    canvas.bind('<Configure>', frame_width)
-    #printers_frame.bind('<Configure>', on_frame_configure)
-
-    printer_button_col = create_printer_name_column(printers, printers_frame)
-    printer_label_col = create_printer_info_column(printers, printers_frame)
-
-    resize_printer_frame(printers_frame, printer_label_col, printer_button_col, 12)
-    printers_frame.update_idletasks()
-
-    set_scroll_region(printer_button_col, canvas)
-    canvas.config(yscrollcommand=vsb.set)
-
+    root.after(8000, printer_frame.update_printers)
     root.mainloop()
